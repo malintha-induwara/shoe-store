987973cf45c04af7b6115cefbe319ace4fee56ca
Induwara <induwara2k@gmail.com>
2025-01-19 12:28:27 +0530

    feat: implement attendance slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,11 @@
-import AuthSlice from '../reducers/AuthSlice'
-import customerSlice from '../reducers/CustomerSlice'
-import itemSlice from '../reducers/ItemSlice'
-import userSlice from '../reducers/UserSlice'
-import staffSlice from '../reducers/StaffSlice'
-import orderSlice from '../reducers/OrderSlice'
-import { configureStore } from '@reduxjs/toolkit';
+import AuthSlice from "../reducers/AuthSlice";
+import customerSlice from "../reducers/CustomerSlice";
+import itemSlice from "../reducers/ItemSlice";
+import userSlice from "../reducers/UserSlice";
+import staffSlice from "../reducers/StaffSlice";
+import orderSlice from "../reducers/OrderSlice";
+import attendanceSlice from "../reducers/AttendanceSlice";
+import { configureStore } from "@reduxjs/toolkit";
 
 const store = configureStore({
   reducer: {
@@ -14,9 +15,9 @@ const store = configureStore({
     user: userSlice,
     staff: staffSlice,
     order: orderSlice,
+    attendance: attendanceSlice,
   },
 });
-
 
 export type RootState = ReturnType<typeof store.getState>;
 export default store;
